7842aa296b596ab910bdf5a45559a8a7c280dff8
Muhammad <bryanedwarding@gmail.com>
2025-10-14 18:56:55 +0100

    no more abbreviation

--- client/src/app/profile/page.tsx
+++ client/src/app/profile/page.tsx
@@ -73,7 +73,7 @@ export default function ProfilePage() {
           {loading ? (
             <p>Loading your reports...</p>
           ) : userReports.length === 0 ? (
-            <p className="text-slate-400">You haven't created any reports yet.</p>
+            <p className="text-slate-400">You have not created any reports yet.</p>
           ) : (
             <div className="space-y-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
               {userReports.map((report) => (
